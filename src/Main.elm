e272b281059054be01f23373fda29f94bfa6b613
Florian Witteler <github@flwi.de>
2020-10-24 23:39:45 +0200

    offsets correct

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,42 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, input, label, text)
-import Html.Attributes as H exposing (..)
-import Html.Events exposing (onInput)
+import Css exposing (..)
+import Css.Animations as Anim exposing (keyframes)
+import FormatNumber exposing (format)
+import FormatNumber.Locales exposing (Decimals(..), usLocale)
+import Html.Styled exposing (Html, button, div, h1, img, input, label, table, td, text, th, toUnstyled, tr)
+import Html.Styled.Attributes as H exposing (..)
+import Html.Styled.Events exposing (onClick, onInput)
+
+
+myFormat =
+    format { usLocale | decimals = Exact 1 }
+
+
+expected : List Int
+expected =
+    [ 38
+    , 166
+    , 286
+    , 416
+    , 542
+    , 672
+    , 798
+    , 928
+    , 1062
+    , 1190
+    , 1318
+    , 1446
+    , 1568
+    , 1696
+    , 1830
+    , 1958
+    , 2128
+    , 2255
+    , 2342
+    , 2470
+    ]
 
 
 
@@ -15,6 +48,7 @@ type alias Model =
     , offset : Pos
     , width : Float
     , height : Float
+    , drawHeight : Float
     }
 
 
@@ -28,14 +62,15 @@ init : ( Model, Cmd Msg )
 init =
     ( { initialOffset =
             { x = 32
-            , y = 38
+            , y = 32
             }
       , offset =
             { x = 60
-            , y = 60
+            , y = 64
             }
       , width = 64
-      , height = 68
+      , height = 64
+      , drawHeight = 64
       }
     , Cmd.none
     )
@@ -53,6 +88,7 @@ type Msg
     | UpdateOffsetY String
     | UpdateWidth String
     | UpdateHeight String
+    | UpdateDrawHeight String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -104,22 +140,21 @@ update msg model =
         UpdateHeight str ->
             ( { model | height = Maybe.withDefault 0 (String.toFloat str) }, Cmd.none )
 
+        UpdateDrawHeight str ->
+            ( { model | drawHeight = Maybe.withDefault 0 (String.toFloat str) }, Cmd.none )
+
         NoOp ->
             ( model, Cmd.none )
 
 
 
 ---- VIEW ----
-
-
-px : Float -> String
-px num =
-    String.fromFloat num ++ "px"
-
-
-pos : Pos -> String
-pos { x, y } =
-    String.join " " [ px x, px y ]
+-- px : Float -> String
+-- px num =
+--     String.fromFloat num ++ "px"
+-- pos : Pos -> String
+-- pos { x, y } =
+--     String.join " " [ px x, px y ]
 
 
 formSlider : Model -> String -> Float -> Float -> (Model -> Float) -> (String -> Msg) -> Html Msg
@@ -135,13 +170,89 @@ formSlider model lbl min max v toMsg =
                 , onInput toMsg
                 ]
                 []
-            , text <| String.fromFloat (v model)
+            , text <| myFormat (v model)
+            , button [ onClick (toMsg (String.fromFloat (v model + 0.5))) ] [ text "+" ]
+            , button [ onClick (toMsg (String.fromFloat (v model - 0.5))) ] [ text "-" ]
             ]
         ]
 
 
-beltNode : Model -> Int -> Int -> Html msg
-beltNode model rowIdx colIdx =
+theme : { secondary : Color, primary : Color }
+theme =
+    { primary = hex "55af6a"
+    , secondary = rgb 250 240 230
+    }
+
+
+cssTransportBeltStyle model =
+    css
+        [ backgroundImage (url "/hr-transport-belt.png")
+        , display inlineBlock
+        , flexShrink (num 0)
+        , flexGrow (num 0)
+        , padding (px 0)
+        , margin (px 0)
+        , backgroundColor theme.primary
+        , Css.width (px model.width)
+        , Css.height (px model.drawHeight)
+        ]
+
+
+numAnimationSteps =
+    16
+
+
+pxFn : Float -> String
+pxFn val =
+    String.fromFloat val ++ "px"
+
+
+yOffsetByRow : Model -> Int -> Float
+yOffsetByRow model rowIdx =
+    model.initialOffset.y + toFloat rowIdx * (model.offset.y + model.height)
+
+
+cssTransportBeltAnimationStyle : Model -> Int -> Html.Styled.Attribute msg
+cssTransportBeltAnimationStyle model rowIdx =
+    let
+        convertPxList : List Float -> String
+        convertPxList xs =
+            xs
+                |> List.map pxFn
+                |> String.join " "
+
+        y =
+            -(yOffsetByRow model rowIdx)
+
+        from =
+            [ -model.initialOffset.x
+            , y
+            ]
+                |> convertPxList
+
+        to =
+            [ -(model.initialOffset.x + numAnimationSteps * model.offset.x + (numAnimationSteps + 1) * model.width)
+            , y
+            ]
+                |> convertPxList
+    in
+    css
+        [ animationName
+            (keyframes
+                [ ( 0, [ Anim.custom "background-position" from ] )
+                , ( 100, [ Anim.custom "background-position" to ] )
+                ]
+            )
+        , animationDuration (ms 266.666666667)
+
+        -- , animationIterationCount infinite --this doesn't compile for some reason
+        , Css.property "animation-iteration-count" "infinite"
+        , Css.property "animation-timing-function" "steps(16)"
+        ]
+
+
+cssBeltNode : Model -> Int -> Int -> Html msg
+cssBeltNode model rowIdx colIdx =
     let
         x =
             -(model.initialOffset.x + (toFloat colIdx * (model.width + model.offset.x)))
@@ -149,60 +260,104 @@ beltNode model rowIdx colIdx =
         y =
             -(model.initialOffset.y + (toFloat rowIdx * (model.height + model.offset.y)))
     in
-    Html.node "sprite"
-        [ class "my-transport-belt"
-        , style "background-position" (pos { x = x, y = y })
-        , style "width" (px model.width)
-        , style "height" (px model.height)
-
-        -- , Html.Attributes.attribute "direction" "e"
+    Html.Styled.node "mySprite"
+        [ cssTransportBeltStyle model
+        , css
+            [ backgroundPosition2 (px x) (px y)
+            ]
         ]
         []
 
 
-animatedBeltNode dir =
+type Direction
+    = West
+    | East
+    | North
+    | South
+
+
+rowIndex dir =
+    case dir of
+        West ->
+            1
+
+        East ->
+            0
+
+        North ->
+            2
+
+        South ->
+            3
+
+
+cssAnimatedBeltNode : Direction -> Model -> Html msg
+cssAnimatedBeltNode dir model =
     div [ class "row" ]
-        [ Html.node "sprite"
-            [ class "transport-belt"
-            , H.attribute "direction" dir
+        [ Html.Styled.node "mySprite"
+            [ cssTransportBeltStyle model
+            , cssTransportBeltAnimationStyle model (rowIndex dir)
             ]
             []
         ]
 
 
-customAnimatedBeltNode dir =
-    div [ class "row" ]
-        [ Html.node "sprite"
-            [ class "transport-belt"
-            , H.attribute "direction" dir
+renderLocationTable : Model -> Html msg
+renderLocationTable model =
+    let
+        calculatedValues : List (Html msg)
+        calculatedValues =
+            expected
+                |> List.indexedMap
+                    (\index e ->
+                        tr []
+                            [ td [] [ text (String.fromInt index) ]
+                            , td [] [ text (String.fromInt e) ]
+                            , td [] [ text (String.fromFloat (yOffsetByRow model index)) ]
+                            ]
+                    )
+    in
+    Html.Styled.table []
+        ([ tr []
+            [ th [] [ text "index" ]
+            , th [] [ text "expected" ]
+            , th [] [ text "actual" ]
             ]
-            []
-        ]
+         ]
+            ++ calculatedValues
+        )
 
 
 view : Model -> Html Msg
 view model =
     div [ class "main" ]
-        [ h1 [] [ text "Factorio belts" ]
-        , formSlider model "initial-offset x" -64 64 (\m -> m.initialOffset.x) UpdateInitialOffsetX
-        , formSlider model "initial-offset y" -64 64 (\m -> m.initialOffset.y) UpdateInitialOffsetY
-        , formSlider model "offset x" -80 80 (\m -> m.offset.x) UpdateOffsetX
-        , formSlider model "offset y" -80 80 (\m -> m.offset.y) UpdateOffsetY
-        , formSlider model "width" -80 80 (\m -> m.width) UpdateWidth
-        , formSlider model "height" -80 80 (\m -> m.height) UpdateHeight
-        , div [ class "row" ]
-            (List.repeat 10 (beltNode model 0 0))
-        , div [ class "row" ]
-            (List.repeat 10 (animatedBeltNode "e"))
-        , div [ class "row" ]
-            (List.repeat
-                10
-                (beltNode model 1 0)
-            )
-        , div [ class "row" ]
-            (List.repeat 10 (animatedBeltNode "w"))
-        , img [ src "/hr-transport-belt.png" ] []
-        ]
+        ([ h1 [] [ text "Factorio belts" ]
+         , formSlider model "initial-offset x" -64 64 (\m -> m.initialOffset.x) UpdateInitialOffsetX
+         , formSlider model "initial-offset y" -64 64 (\m -> m.initialOffset.y) UpdateInitialOffsetY
+         , formSlider model "offset x" -80 80 (\m -> m.offset.x) UpdateOffsetX
+         , formSlider model "offset y" -80 80 (\m -> m.offset.y) UpdateOffsetY
+         , formSlider model "width" -80 80 (\m -> m.width) UpdateWidth
+         , formSlider model "height" -80 80 (\m -> m.height) UpdateHeight
+         , formSlider model "draw-height" -80 80 (\m -> m.drawHeight) UpdateDrawHeight
+
+         --  , div [ class "row" ]
+         --     (List.repeat 3 (cssBeltNode model 0 0))
+         , div [ class "row" ]
+            (List.repeat 3 (cssAnimatedBeltNode West model) ++ List.repeat 3 (cssAnimatedBeltNode East model))
+         , div []
+            [ renderLocationTable model
+            ]
+
+         --  , div [ class "row" ]
+         --     (List.repeat 3 (cssBeltNode model 1 0))
+         , div [ class "row" ]
+            (List.repeat 3 (cssAnimatedBeltNode East model) ++ List.repeat 3 (cssAnimatedBeltNode West model))
+         ]
+            ++ List.repeat 3 (div [ class "row" ] [ cssAnimatedBeltNode North model, cssAnimatedBeltNode East model, cssAnimatedBeltNode South model, cssAnimatedBeltNode West model ])
+            ++ List.repeat 3 (div [ class "row" ] [ cssAnimatedBeltNode South model ])
+            ++ [ img [ src "/hr-transport-belt.png" ] []
+               ]
+        )
 
 
 
@@ -212,7 +367,7 @@ view model =
 main : Program () Model Msg
 main =
     Browser.element
-        { view = view
+        { view = view >> toUnstyled
         , init = \_ -> init
         , update = update
         , subscriptions = always Sub.none
