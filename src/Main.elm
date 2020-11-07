0f76bdf5dc459312ae9336df91500002f0945e10
Florian Witteler <github@flwi.de>
2020-11-07 18:14:33 +0100

    rendering extended to curves. not perfect, but ok for now.
    - Slight glitch in circles.
    - gap between two horizontal belts bigger than gap between two vertical belts

--- src/Main.elm
+++ src/Main.elm
@@ -10,9 +10,11 @@ import Canvas.Texture exposing (..)
 import Color
 import Css exposing (local)
 import Dict exposing (Dict)
-import Html exposing (Html)
+import Dict.Extra
+import Html exposing (Html, a)
 import Html.Attributes exposing (style)
 import List.Extra
+import Svg.Styled.Attributes exposing (direction)
 
 
 type alias Model =
@@ -28,7 +30,7 @@ type Load a
 
 
 type alias Sprites =
-    { animationFrames : List Texture
+    { animationFrames : Dict String (List Texture)
     }
 
 
@@ -44,10 +46,11 @@ init =
     )
 
 
+offsets : { initialOffset : { x : Float, y : Float }, offset : { x : Float, y : Float }, width : Float, height : Float }
 offsets =
     { initialOffset =
         { x = 32
-        , y = 38
+        , y = 32
         }
     , offset =
         { x = 64
@@ -58,8 +61,8 @@ offsets =
     }
 
 
-createTextureList : Texture -> List Texture
-createTextureList texture =
+createTextureDict : Texture -> Dict String (List Texture)
+createTextureDict texture =
     let
         o =
             offsets
@@ -67,11 +70,29 @@ createTextureList texture =
         x =
             o.initialOffset.x
 
-        y =
-            o.initialOffset.y
+        texturesByDirectionName : String -> List Texture
+        texturesByDirectionName directionName =
+            let
+                directionIndex =
+                    beltRenderDirectionsByString
+                        |> Dict.get directionName
+                        |> Maybe.map Tuple.first
+                        |> Maybe.withDefault -1
+            in
+            List.range 0 (numAnimationFrames - 1)
+                |> List.map
+                    (\i ->
+                        sprite
+                            { x = x + (o.offset.x + o.width) * toFloat i - 1
+                            , y = o.initialOffset.y + toFloat directionIndex * (o.height + o.offset.y)
+                            , width = o.width + 1
+                            , height = o.height
+                            }
+                            texture
+                    )
     in
-    List.range 0 (numAnimationFrames - 1)
-        |> List.map (\i -> sprite { x = x + (o.offset.x + o.width) * toFloat i - 1, y = o.initialOffset.y, width = o.width + 1, height = o.height } texture)
+    beltRenderDirectionsByString
+        |> Dict.map (\dir _ -> texturesByDirectionName dir)
 
 
 numAnimationFrames =
@@ -82,31 +103,77 @@ framerate =
     60 / 30
 
 
-type Direction
-    = West
+type BlueprintDirection
+    = North
+    | Northeast
     | East
-    | North
+    | Southeast
     | South
+    | Southwest
+    | West
+    | Northwest
 
 
-rowIndexByDirection dir =
-    case dir of
-        West ->
-            1
+type BeltRenderDirection
+    = NorthEast
+    | NorthSouth
+    | NorthWest
+    | EastNorth
+    | EastSouth
+    | EastWest
+    | SouthNorth
+    | SouthEast
+    | SouthWest
+    | WestNorth
+    | WestEast
+    | WestSouth
 
-        East ->
-            0
 
-        North ->
-            2
+factorioBlueprintDirection =
+    Dict.fromList
+        [ ( 0, North )
+        , ( 1, Northeast )
+        , ( 2, East )
+        , ( 3, Southeast )
+        , ( 4, South )
+        , ( 5, Southwest )
+        , ( 6, West )
+        , ( 7, Northwest )
+        ]
 
-        South ->
-            3
+
+beltRenderDirectionsByIdx : Dict Int ( BeltRenderDirection, String )
+beltRenderDirectionsByIdx =
+    Dict.fromList
+        [ ( 1, ( EastWest, "EastWest" ) )
+        , ( 0, ( WestEast, "WestEast" ) )
+        , ( 2, ( SouthNorth, "SouthNorth" ) )
+        , ( 3, ( NorthSouth, "NorthSouth" ) )
+        , ( 5, ( NorthEast, "NorthEast" ) )
+        , ( 8, ( SouthEast, "SouthEast" ) )
+        , ( 10, ( SouthWest, "SouthWest" ) )
+        , ( 7, ( NorthWest, "NorthWest" ) )
+        , ( 4, ( EastNorth, "EastNorth" ) )
+        , ( 9, ( EastSouth, "EastSouth" ) )
+        , ( 6, ( WestNorth, "WestNorth" ) )
+        , ( 11, ( WestSouth, "WestSouth" ) )
+        ]
 
 
-renderBeltAnimation : Model -> Pos -> List Renderable
-renderBeltAnimation model pos =
+beltRenderDirectionsByString : Dict String ( Int, BeltRenderDirection )
+beltRenderDirectionsByString =
+    beltRenderDirectionsByIdx
+        |> Dict.toList
+        |> List.map (\( idx, ( dir, dirString ) ) -> ( dirString, ( idx, dir ) ))
+        |> Dict.fromList
+
+
+renderBeltAnimation : Model -> GameEntity -> List Renderable
+renderBeltAnimation model { x, y, direction } =
     let
+        pos =
+            { x = x * offsets.width, y = y * offsets.height }
+
         renderFrame =
             round (toFloat model.frame / framerate)
 
@@ -117,16 +184,94 @@ renderBeltAnimation model pos =
 
             else
                 0
+
+        directionString =
+            beltRenderDirectionsByIdx
+                |> Dict.values
+                |> List.Extra.find (\( dir, str ) -> dir == direction)
+                |> Maybe.map Tuple.second
+                |> Maybe.withDefault "broken"
     in
     case model.sprites of
         Success sprites ->
             sprites.animationFrames
+                |> Dict.get directionString
+                |> Maybe.withDefault []
                 |> List.Extra.getAt animationIndex
                 |> Maybe.map (renderBelt pos)
                 |> Maybe.withDefault []
 
         _ ->
             []
+
+
+type alias GameEntity =
+    { x : Float, y : Float, direction : BeltRenderDirection }
+
+
+type RotationDirection
+    = Clockwise
+    | CounterClockwise
+
+
+circle : Pos -> RotationDirection -> List GameEntity
+circle offset rot =
+    [ { x = 0 + offset.x
+      , y = 0 + offset.y
+      , direction =
+            if rot == Clockwise then
+                SouthEast
+
+            else
+                EastSouth
+      }
+    , { x = 1 + offset.x
+      , y = 0 + offset.y
+      , direction =
+            if rot == Clockwise then
+                WestSouth
+
+            else
+                SouthWest
+      }
+    , { x = 1 + offset.x
+      , y = 1 + offset.y
+      , direction =
+            if rot == Clockwise then
+                NorthWest
+
+            else
+                WestNorth
+      }
+    , { x = 0 + offset.x
+      , y = 1 + offset.y
+      , direction =
+            if rot == Clockwise then
+                EastNorth
+
+            else
+                NorthEast
+      }
+    ]
+
+
+straight : BeltRenderDirection -> Float -> List GameEntity
+straight dir y =
+    [ { x = 0, y = y, direction = dir }
+    , { x = 1, y = y, direction = dir }
+    , { x = 2, y = y, direction = dir }
+    , { x = 3, y = y, direction = dir }
+    ]
+
+
+straightVertical : BeltRenderDirection -> Float -> List GameEntity
+straightVertical dir x =
+    [ { x = x, y = 0, direction = dir }
+    , { x = x, y = 1, direction = dir }
+    , { x = x, y = 2, direction = dir }
+    , { x = x, y = 3, direction = dir }
+    , { x = x, y = 4, direction = dir }
+    ]
 
 
 view : Model -> Html Msg
@@ -147,9 +292,12 @@ view model =
         ([ shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
          , renderSquare
          ]
-            ++ renderBeltAnimation model { x = 0, y = toFloat 0 }
-            ++ renderBeltAnimation model { x = offsets.width, y = toFloat 0 }
-            ++ renderBeltAnimation model { x = toFloat 2 * offsets.width, y = toFloat 0 }
+            ++ (circle { x = 0, y = 0 } Clockwise |> List.concatMap (renderBeltAnimation model))
+            ++ (circle { x = 2, y = 0 } CounterClockwise |> List.concatMap (renderBeltAnimation model))
+            ++ (straight WestEast 3 |> List.concatMap (renderBeltAnimation model))
+            ++ (straight EastWest 4 |> List.concatMap (renderBeltAnimation model))
+            ++ (straightVertical NorthSouth 4 |> List.concatMap (renderBeltAnimation model))
+            ++ (straightVertical SouthNorth 5 |> List.concatMap (renderBeltAnimation model))
         )
 
 
@@ -180,7 +328,7 @@ update msg model =
             )
 
         TextureLoaded (Just texture) ->
-            ( { model | sprites = Success { animationFrames = createTextureList texture } }, Cmd.none )
+            ( { model | sprites = Success { animationFrames = createTextureDict texture } }, Cmd.none )
 
         TextureLoaded Nothing ->
             ( model, Cmd.none )
